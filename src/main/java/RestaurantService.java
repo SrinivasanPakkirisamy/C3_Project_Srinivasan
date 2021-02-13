080f66b0dcf83fc4bbb10ea8e32cd4d7759e5249
Srinivasan Pakkirisamy <srinivasan.apps@gmail.com>
2021-02-13 18:35:49 -0500

    Restaurant Finder Initial Commit

---
+++ src/main/java/RestaurantService.java
@@ -0,0 +1,34 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName) throws RestaurantNotFoundException {
+        //Compare the restaurant name with list of available restaurants and return the restaurant if available otherwise throw an exception
+        for (Restaurant restaurant : getRestaurants()) {
+            if (restaurant.getName().equals(restaurantName)) {
+                return restaurant;
+            }
+        }
+        throw new RestaurantNotFoundException(restaurantName);
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws RestaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
